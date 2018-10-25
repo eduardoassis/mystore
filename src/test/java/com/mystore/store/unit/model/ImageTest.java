bf83048b3581465c59c133e4becd079ce050b127
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-10-24 23:48:01 -0300

    [MODEL] Coding unit test to image model

--- src/test/java/com/mystore/store/unit/model/ImageTest.java
+++ src/test/java/com/mystore/store/unit/model/ImageTest.java
@@ -1,4 +1,33 @@
 package com.mystore.store.unit.model;
 
+import com.mystore.store.model.Image;
+import com.mystore.store.repository.ImageRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DataJpaTest
 public class ImageTest {
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    private Long id;
+
+    @Test
+    public void shouldSaveImage() {
+
+        Image image = new Image();
+        image.setType("png");
+        imageRepository.save(image);
+        assertNotNull(image.getId());
+
+    }
 }
