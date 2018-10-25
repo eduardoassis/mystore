6807586032c8318dc148b9fc4ecfd2d4185bbfee
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-10-25 00:29:01 -0300

    [MODEL] Implementing more cases of test to image

--- src/test/java/com/mystore/store/unit/model/ImageTest.java
+++ src/test/java/com/mystore/store/unit/model/ImageTest.java
@@ -2,6 +2,7 @@ package com.mystore.store.unit.model;
 
 import com.mystore.store.model.Image;
 import com.mystore.store.repository.ImageRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,7 +22,22 @@ public class ImageTest {
     @Autowired
     private ImageRepository imageRepository;
 
-    private Long id;
+    private Long idSavedImage;
+    private Image imageToDelete;
+
+
+    @Before
+    public void before() {
+        Image i = new Image();
+        i.setType("jpeg");
+        imageRepository.save(i);
+        assertNotNull(i.getId());
+        idSavedImage = i.getId();
+
+        imageToDelete = new Image();
+        imageToDelete.setType("jpeg");
+        imageRepository.save(imageToDelete);
+    }
 
     @Test
     public void shouldSaveImage() {
@@ -29,5 +47,21 @@ public class ImageTest {
         imageRepository.save(image);
         assertNotNull(image.getId());
 
+    }
+
+    @Test
+    public void shouldRetrieveImage() {
+        Optional<Image> image = imageRepository.findById(idSavedImage);
+        assertTrue(image.isPresent());
+        assertEquals(idSavedImage, image.get().getId());
+    }
+
+    @Test
+    public void shouldDeleteImage() {
+        Long id = imageToDelete.getId();
+        imageRepository.delete(imageToDelete);
+
+        Optional<Image> image = imageRepository.findById(id);
+        assertFalse(image.isPresent());
     }
 }
