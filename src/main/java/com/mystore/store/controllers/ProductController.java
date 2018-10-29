f4e7749ca5a2f848bb1a4b08881db0694b7924ef
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-10-29 19:26:48 -0300

    Created endpoint to retrieve a product by id

--- src/main/java/com/mystore/store/controllers/ProductController.java
+++ src/main/java/com/mystore/store/controllers/ProductController.java
@@ -1,18 +1,22 @@
 package com.mystore.store.controllers;
 
+import com.mystore.store.model.Product;
 import com.mystore.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "products")
@@ -28,5 +32,22 @@ public class ProductController {
     public Collection getAllProducts() {
         List products = (List) productRepository.findAll();
         return products;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response findProduct(@PathParam("id") Long id) {
+
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            return Response
+                    .ok()
+                    .entity(product.get())
+                    .build();
+        }
+
+        return Response.noContent().build();
     }
 }
